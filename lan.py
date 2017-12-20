7b719fddbdde0c1813eeb5980e04bc669f6a7923
languoxing <languoxing@jd.com>
2017-12-20 13:53:27 +0800

    no commits

--- lan.py
+++ lan.py
@@ -4,6 +4,7 @@ Created on Fri Apr  7 23:55:37 2017
 
 @author: lankuohsing
 """
+'''
 import numpy as np
 a=1
 b=2
@@ -12,3 +13,6 @@ c=np.mat([[1,2]])
 
 d=np.mat([[3], [4]])
 print(c*d)
+'''
+# In[]
+import tensorflow as tf
