f38b0f0c1523a412de2f5e1ac5b995e020861ea9
languoxing <languoxing@126.com>
2018-01-16 17:53:29 +0800

    training_step=5000 hidden_size=30

--- LSTM/run5.sh
+++
@@ -1,3 +0,0 @@
-source ~/tensorflow/bin/activate      # bash, sh, ksh, or zsh
-CUDA_VISIBLE_DEVICES=0 python stock_predict_5.py  # Uses GPU 0.
-
--- LSTM/run6.sh
+++
@@ -1,3 +0,0 @@
-source ~/tensorflow/bin/activate      # bash, sh, ksh, or zsh
-CUDA_VISIBLE_DEVICES=1 python stock_predict_6.py  # Uses GPU 1.
-
