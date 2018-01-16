f8feaf4b7a08dbe35eb163b65da76e7e93e1df39
languoxing <languoxing@126.com>
2018-01-16 19:18:03 +0800

    no commits

--- LSTM/timeseries/stock/run6.sh
+++ LSTM/timeseries/stock/run6.sh
@@ -1,3 +1,3 @@
 source ~/tensorflow/bin/activate      # bash, sh, ksh, or zsh
-CUDA_VISIBLE_DEVICES=1 python stock_predict_6.py --num_layers=2 --hidden_size=30 training_steps=50000 # Uses GPU 1.
+CUDA_VISIBLE_DEVICES=1 python stock_predict_6.py --num_layers=2 --hidden_size=30 --training_steps=50000 # Uses GPU 1.
 
