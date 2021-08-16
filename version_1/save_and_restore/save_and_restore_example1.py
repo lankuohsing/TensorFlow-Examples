a66be9a8e2b5f55fbed871f8432154d164207a5b
lankuohsing <lankuohsing@163.com>
2021-08-17 00:20:41 +0800

    add save and restore examples

--- version_1/save_and_restore/save_and_restore_example1.py
+++ version_1/save_and_restore/save_and_restore_example1.py
@@ -31,3 +31,22 @@ with tf.Session() as sess:
     # Save the variables to disk.
     save_path = saver.save(sess, "./tmp/model.ckpt")
     print("Model saved in path: %s" % save_path)
+# In[]
+tf.reset_default_graph()
+
+# Create some variables.
+v1 = tf.get_variable("v1", shape=[3])
+v2 = tf.get_variable("v2", shape=[5])
+
+# Add ops to save and restore all the variables.
+saver = tf.train.Saver()
+
+# Later, launch the model, use the saver to restore variables from disk, and
+# do some work with the model.
+with tf.Session() as sess:
+  # Restore variables from disk.
+  saver.restore(sess, "/tmp/model.ckpt")
+  print("Model restored.")
+  # Check the values of the variables
+  print("v1 : %s" % v1.eval())
+  print("v2 : %s" % v2.eval())
